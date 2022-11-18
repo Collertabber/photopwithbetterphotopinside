cf51888f5651b07275455cc242ababf1098bf885
Collertabber <84198237+Collertabber@users.noreply.github.com>
2022-11-17 23:51:22 -0500

    Update bookmarklet_test.js

--- bookmarklet_test.js
+++ bookmarklet_test.js
@@ -1,18 +1,27 @@
+function notifaction(one, two, three) {
+createNotifStuff({
+  title: one,
+  content: two,
+  time: three
+})
+}
+
+function sendNoti() {
+notifaction(`Siri's Alt Url Loaded!`, `(:1040793007722602547) 's Alt url has been loaded :D`, 5000);
+}
+
 let person = prompt("Enable Beta BP?","N");
 person = person.toLowerCase();
 if (person == "n") {
     loadScript('https://betterphotop.ga/betterphotop/script.js')
     alert("Loading Public Version");
+    setTimeout(sendNoti, 4000);
 } else {
     loadScript("https://betterphotop.ga/betterphotopTesting/script.js");
     alert("Loading Beta Version");
     let name = prompt("Password?");
     if (name != null) {
     window.localStorage.setItem('pass',name);
+    setTimeout(sendNoti, 4000);
     }
 }
-createNotifStuff({
-  title: `Siri's Alt Url Loaded!`,
-  content: `(:1040793007722602547) 's Alt url has been loaded :D`,
-  time: 5000
-})
